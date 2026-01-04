efce0ea2389612c5d6c7a5978c73f83adaa51886
Eden Van-Dycen <edenvandycen@gmail.com>
2026-01-04 17:00:43 +0300

    Day 1/1

--- 4-01-26/main.js
+++ 4-01-26/main.js
@@ -1,1 +1,26 @@
+let number = 5;
+const leftBorderWidth = 1;
 
+number = 10;
+console.log(number);
+
+const obj = {
+  a: 50
+};
+obj.a = 10
+console.log(obj);
+
+//Request
+// data
+// response
+//Серверные части(запросы)
+
+// snake_case
+//UPPERS_SNAKE_CASE Называют константы которые нельзя переопределять 
+//Kebab-case
+//PascalCase 
+
+let COLOR_RED = '#F00'; // Константы которые нельзя менять без соглосования с командой 
+
+const _apiBase = 'https://gateway.marvel.com:443/v1/public/'; // Такие переменные нельзя трогать без согласования с командой 
+const _apiKey = 'apikey=d4eecb0c66dedbfae4aeb45d312fc1df'; // Такие переменные нельзя трогать без согласования с командой 
